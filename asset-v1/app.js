5a474be21921986c105f9ee4f6c5ef0e9b130318
Thorley-Arya Öner <oner.thorley@gmail.com>
2023-09-16 09:35:01 -0500

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,10 +1,37 @@
 /* 0.5.7 2021-12-27 16:37:09 */
 var app = new function() {
-	this.name = "Sunset", this.version = "1", this.date = "2023", this.folder = "asset-v1/", this.looptime = 6857, this.bpm = 140, this.totalframe = 328, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !1, this.bonusendloopA = !1, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#121212", this.col0 = "#919191", this.col1 = "#5f5f5f", this.col2 = "#4b4b4b", this.col3 = "#373737", this.col4 = "#232323", this.animearray = [{
-		name: "1_kick",
-		color: "3C5E6F",
-		uniqsnd: !0
-	}], this.bonusarray = [ /*No bonuses?*/ ];
+	this.name = "TestBox", 
+	this.version = "1_TestBox",
+	this.date = "2023", 
+	this.folder = "asset-v1/", 
+	this.looptime = 6857, 
+	this.bpm = 140, 
+	this.totalframe = 328, 
+	this.nbpolo = 7, 
+	this.nbloopbonus = 2, 
+	this.bonusloopA = !1, 
+	this.bonusendloopA = !1, 
+	this.recmaxloop = 34, 
+	this.recminloop = 4, 
+	this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, 
+	this.spritepolo = "polo-sprite.png", 
+	this.spritepolotshirt = "polo-sprite-tshirt.png", 
+	this.spritepicto = "game-picto.png", 
+	this.colBck = "#121212", 
+	this.col0 = "#919191", 
+	this.col1 = "#5f5f5f", 
+	this.col2 = "#4b4b4b", 
+	this.col3 = "#373737", 
+	this.col4 = "#232323", 
+	
+	this.animearray = [
+	    {
+		name: "1_test",
+		color: "000000",
+		uniqsnd: !1
+	    }
+	], this.bonusarray = [ /*No bonuses?*/ ];
+	
 	for (var n = 0, o = this.animearray.length; n < o; n++) {
 		var a = this.animearray[n].name;
 		this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json"
