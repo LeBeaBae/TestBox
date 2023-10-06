37aa5fdf41dc6e8f457d52723f2c0d1ed138afe0
LeBeaBae <oner.thorley@gmail.com>
2023-10-06 17:14:53 -0500

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,114 +1,90 @@
-/* 0.5.7 2021-12-27 16:37:09 */
+/* Finally, some good fucking food */
 var app = new function() {
-	this.name = "TestBox", 
-	this.version = "1_TestBox",
-	this.date = "2023", 
-	this.folder = "asset-v1/", 
-	this.looptime = 6e3, 
-	this.bpm = 80, 
-	this.totalframe = 358, 
-	this.nbpolo = 7, 
-	this.nbloopbonus = 2, 
-	this.bonusloopA = !1, 
-	this.bonusendloopA = !1, 
-	this.recmaxloop = 34, 
-	this.recminloop = 4, 
-	this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, 
-	this.spritepolo = "polo-sprite.png", 
-	this.spritepolotshirt = "polo-sprite-tshirt.png", 
-	this.spritepicto = "game-picto.png", 
-	this.colBck = "#121212", 
-	this.col0 = "#919191", 
-	this.col1 = "#5f5f5f", 
-	this.col2 = "#4b4b4b", 
-	this.col3 = "#373737", 
-	this.col4 = "#232323", 
-	
-	this.animearray = [
-	    {
-		name: "1_Kikaz",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "2_deux",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "3_kosh",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "4_shpok",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "5_tom",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "6_buzz",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "7_scratch",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "8_trill",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "9_boombox",
-		color: "828282",
-		uniqsnd: !1
-	}, {
-		name: "10_uh",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "11_nugu",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "12_guit",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "13_tromp",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "14_pouin",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "15_tung",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "16_aoun",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "17_puoha",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "18_get",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "19_tellme",
-		color: "828282",
-		uniqsnd: !0
-	}, {
-		name: "20_make",
-		color: "828282",
-		uniqsnd: !1
-	}], this.bonusarray = [ /*No bonuses?*/ ];
-	
-	for (var n = 0, o = this.animearray.length; n < o; n++) {
-		var a = this.animearray[n].name;
-		this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json"
-	}
+    this.name = "The Last Day", this.version = "Unreleased v1", this.date = "2023", this.folder = "asset-v1/", this.looptime = 11750, this.bpm = 100, this.totalframe = 570, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 96, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#0f1521", this.col0 = "#828da0", this.col1 = "#596988", this.col2 = "#384863", this.col3 = "#202a3c", this.col4 = "#131a2a", this.animearray = [{
+        name: "01kick",
+        color: "af8553",
+        uniqsnd: !1
+    }, {
+        name: "02snare",
+        color: "af8553",
+        uniqsnd: !1
+    }, {
+        name: "03clic",
+        color: "af8553",
+        uniqsnd: !1
+    }, {
+        name: "04klop",
+        color: "af8553",
+        uniqsnd: !0
+    }, {
+        name: "05tombreak",
+        color: "af8553",
+        uniqsnd: !1
+    }, {
+        name: "06bass",
+        color: "10a3b7",
+        uniqsnd: !1
+    }, {
+        name: "07mmmhah",
+        color: "10a3b7",
+        uniqsnd: !0
+    }, {
+        name: "08tututoung",
+        color: "10a3b7",
+        uniqsnd: !1
+    }, {
+        name: "09boowop",
+        color: "10a3b7",
+        uniqsnd: !0
+    }, {
+        name: "10woah",
+        color: "10a3b7",
+        uniqsnd: !1
+    }, {
+        name: "11nugununugu",
+        color: "fe0849",
+        uniqsnd: !1
+    }, {
+        name: "12string",
+        color: "fe0849",
+        uniqsnd: !1
+    }, {
+        name: "13lowarp",
+        color: "fe0849",
+        uniqsnd: !1
+    }, {
+        name: "14settoarp",
+        color: "fe0849",
+        uniqsnd: !1
+    }, {
+        name: "15duuduuduu",
+        color: "fe0849",
+        uniqsnd: !0
+    }, {
+        name: "16somuch",
+        color: "9573fe",
+        uniqsnd: !1
+    }, {
+        name: "17casting",
+        color: "9573fe",
+        uniqsnd: !1
+    }, {
+        name: "18ahwoo",
+        color: "9573fe",
+        uniqsnd: !1
+    }, {
+        name: "19lastday",
+        color: "9573fe",
+        uniqsnd: !1
+    }, {
+        name: "20mind",
+        color: "9573fe",
+        uniqsnd: !1
+    }], this.bonusarray = [{
+        name: "The Last Day",
+        src: "v1-b1-thelastday-hb.mp4",
+        code: "1,2,6,11,13",
+        sound: "bonus-thelastday",
+        aspire: "aspire-thelastday"
+    }]
 };
