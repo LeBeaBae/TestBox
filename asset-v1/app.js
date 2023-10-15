2363a46c6afcd54862001b0f5f493207f7259780
LeBeaBae <50959303+LeBeaBae@users.noreply.github.com>
2023-10-14 22:43:52 -0500

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -3,7 +3,7 @@ var app = new function() {
     this.name = "Pushing Life", this.version = "A", this.date = "2022", this.folder = "asset-v1/", this.looptime = 8000, this.bpm = 120, this.totalframe = 384, this.nbpolo = 7, this.nbloopbonus = 3, this.bonusloopA = !0, this.bonusendloopA = !0, this.recmaxloop = 68, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#121212", this.col0 = "#1b1851", this.col1 = "#1b1851", this.col2 = "#12103d", this.col3 = "#0d0b30", this.col4 = "#232323", this.animearray = [{
         name: "1_kick",
         color: "b816ad",
-        uniqsnd: !1
+        uniqsnd: !0
     }, {
         name: "3_snap",
         color: "b816ad",
